6a89b4a331fbf201de3b3036aac565a737b619e5
Andrew Fitzgibbon <awf@microsoft.com>
2021-10-12 14:05:22 +0100

    Improve docs

--- setup.ps1
+++ setup.ps1
@@ -2,6 +2,10 @@ $pkg = Get-Package HtmlAgilityPack
 
 if (!$pkg) {
     $pkg = Install-Package -verbose -scope CurrentUser HtmlAgilityPack
+    # Errors here?
+    # Consider:
+    # Register-PackageSource -Name MyNuGet -Location https://www.nuget.org/api/v2 -ProviderName NuGet 
+    # Install-Package -scope CurrentUser HtmlAgilityPack -SkipDependencies
 }
 
 $pkgdir = (get-item $pkg.source).Directory
